892ed61fedb730d41f38831f1ef82a57474f15d9
Bicky <bicky@MacBook-Pro-de-Bicky.local>
2022-11-24 12:45:48 -0600

    done

--- src/App.js
+++ src/App.js
@@ -4,37 +4,74 @@ import Historia from './componentes/Historia';
 function App() {
   return (
     <div className="App">
-      <h1>Sprint Planning</h1>
+      <h1>Sprints Proyecto II</h1>
       <div className='contenedor-sprint'>
         <div>
-          <p className='todo'>To Do</p>
-          <p className='doing'>Doing</p>
-          <p className='done'>Done!</p>
+          <h2 className='done'>Done!</h2>
         </div>
         <div className='fila'>
-          <Historia
-          diseno='diseño'
-          desarrollo='desarrollo'
-          numero='SCW 4'
-          contenido='4.1 Diseñar sistema de pago'
-        />
-        <Historia
-          desarrollo='desarrollo'
-          diseno='diseño'
-          numero='SCW 3'
-          contenido='3.1 Integrar calendario Google al sitio web'
-        />
         <Historia
           documentacion='documentación'
           numero='SCW 1'
           contenido='1.1 Recopilación de información de abogados (reseña curricular y fotos)'
         />
+        <Historia
+          documentacion='documentación'
+          numero='SCW 1'
+          contenido='1.2 Diseñar un catálogo de los Abogados (reseña curricular, foto y especialidad)'
+        />
+        <Historia
+          done='Done!'
+          desarrollo='desarrollo'
+          numero='SCW 1'
+          contenido='1.3 Contratar Base de datos y hosting'
+        />
+        </div>
+        <div className='fila'>
+        <Historia
+          done='Done!'
+          diseno='diseño'
+          numero='SCW 1'
+          contenido='1.4 Maquetar el diseño del sitio web.'
+        />
+        <Historia
+          done='Done!'
+          diseno='diseño'
+          desarrollo='desarrollo'
+          numero='SCW 2'
+          contenido='2.1 Diseñar un formulario de registro de clientes'
+        />
+        <Historia
+          desarrollo='desarrollo'
+          numero='SCW 2'
+          contenido='2.2 Programar formulario de registro en HTML'
+        />
+        </div>
+        <div className='fila'>
+        <Historia
+          diseno='diseño'
+          desarrollo='desarrollo'
+          numero='SCW 2'
+          contenido='2.3 Diseñar la interfaz de formulario de registro en CSS'
+        />
+        <Historia
+          documentacion='documentación'
+          numero='SCW 2'
+          contenido='2.4 Investigar y organizar la información que se requiera para la Base de Datos'
+        />
+        <Historia
+          done='Done!'
+          desarrollo='desarrollo'
+          numero='SCW 2'
+          contenido='2.5 Programar la Base de Datos'
+        />
         </div>
         <div className='fila'>
         <Historia
           desarrollo='desarrollo'
-          numero='SCW 4'
-          contenido='4.2 Programación backend del sistema de pago de la cita agendada de los clientes'
+          diseno='diseño'
+          numero='SCW 3'
+          contenido='3.1 Integrar calendario Google al sitio web'
         />
         <Historia
           desarrollo='desarrollo'
@@ -43,9 +80,30 @@ function App() {
           contenido='3.2 Crear botón calendario al sitio web'
         />
         <Historia
-          documentacion='documentación'
-          numero='SCW 1'
-          contenido='1.2 Diseñar un catálogo de los Abogados (reseña curricular, foto y especialidad)'
+          doing='Doing'
+          desarrollo='desarrollo'
+          numero='SCW 3'
+          contenido='3.3 Integrar a HTML codificación JAVASCRIPT'
+        />
+        </div>
+        <div className='fila'>
+        <Historia
+          doing='Doing'
+          desarrollo='desarrollo'
+          testing='testing'
+          numero='SCW 3'
+          contenido='3.4 Pruebas frontend-backend'
+        />
+          <Historia
+          diseno='diseño'
+          desarrollo='desarrollo'
+          numero='SCW 4'
+          contenido='4.1 Diseñar sistema de pago'
+        />
+        <Historia
+          desarrollo='desarrollo'
+          numero='SCW 4'
+          contenido='4.2 Programación backend del sistema de pago de la cita agendada de los clientes'
         />
         </div>
         <div className='fila'>
@@ -57,20 +115,6 @@ function App() {
           contenido='4.3 Pruebas frontend-backend'
         />
         <Historia
-          doing='Doing'
-          desarrollo='desarrollo'
-          numero='SCW 3'
-          contenido='3.3 Integrar a HTML codificación JAVASCRIPT'
-        />
-        <Historia
-          done='Done!'
-          desarrollo='desarrollo'
-          numero='SCW 1'
-          contenido='1.3 Contratar Base de datos y hosting'
-        />
-        </div>
-        <div className='fila'>
-        <Historia
           todo='To Do'
           desarrollo='desarrollo'
           documentacion='documentación'
@@ -78,34 +122,10 @@ function App() {
           contenido='5.1 Investigar cómo automatizar correos electrónicos de confirmación'
         />
         <Historia
-          doing='Doing'
-          desarrollo='desarrollo'
-          testing='testing'
-          numero='SCW 3'
-          contenido='3.4 Pruebas frontend-backend'
-        />
-        <Historia
-          done='Done!'
-          diseno='diseño'
-          numero='SCW 1'
-          contenido='1.4 Maquetar el diseño del sitio web.'
-        />
-        </div>
-        <div className='fila'>
-        <Historia
           todo='To Do'
           desarrollo='desarrollo'
           numero='SCW 5'
           contenido='5.2 Diseñar correo electrónico transaccional'
-        />
-        <Historia
-        />
-        <Historia
-          done='Done!'
-          diseno='diseño'
-          desarrollo='desarrollo'
-          numero='SCW 2'
-          contenido='2.1 Diseñar un formulario de registro de clientes'
         />
         </div>
         <div className='fila'>
@@ -115,43 +135,17 @@ function App() {
           contenido='5.3 Configurar correo electrónico transaccional y activarlo'
         />
         <Historia
-        />
-        <Historia
-          desarrollo='desarrollo'
-          numero='SCW 2'
-          contenido='2.2 Programar formulario de registro en HTML'
-        />
-        </div>
-        <div className='fila'>
-        <Historia
           desarrollo='desarrollo'
           testing='testing'
           numero='SCW 5'
           contenido='5.4 Pruebas frontend-backend'
         />
         <Historia
-        />
-        <Historia
-          diseno='diseño'
-          desarrollo='desarrollo'
-          numero='SCW 2'
-          contenido='2.3 Diseñar la interfaz de formulario de registro en CSS'
-        />
-        </div>
-        <div className='fila'>
-        <Historia
           desarrollo='desarrollo'
           numero='SCW 5'
           contenido='5.5 Alojamiento del proyecto en hosting'
         />
-        <Historia
-        />
-        <Historia
-          documentacion='documentación'
-          numero='SCW 2'
-          contenido='2.4 Investigar y organizar la información que se requiera para la Base de Datos'
-        />
-        </div>
+         </div>
         <div className='fila'>
         <Historia
           todo='To Do'
@@ -162,10 +156,6 @@ function App() {
         <Historia
         />
         <Historia
-          done='Done!'
-          desarrollo='desarrollo'
-          numero='SCW 2'
-          contenido='2.5 Programar la Base de Datos'
         />
         </div>
       </div>
